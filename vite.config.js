633d764cb86d043e4fa5279d8930e7fc6e891fea
Temi-suwa18 <271503102+Temi-suwa18@users.noreply.github.com>
2026-05-16 16:52:30 +0100

    docs: add deployment workflow

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/frontend/', // This should match the repo name if deploying to GitHub Pages
+})
